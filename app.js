272d2a8423d266b64d259ba961ae89b9ef5652ab
nogy <norven.norov@gmail.com>
2020-03-14 22:55:54 +0800

    oruulsan utgiig delgetsnees avah bolomjtoi bolloo

--- app.js
+++ app.js
@@ -1,18 +1,42 @@
 //Дэлгэцтэй ажиллах controller
-var uiController = (function() {})();
+var uiController = (function() {
+  var DOMstring = {
+    inputType: ".add__type",
+    inputDesc: ".add__description",
+    inputValue: ".add__value",
+    addBtn: ".add__btn"
+  };
+
+  return {
+    inputValue: function() {
+      return {
+        Type: document.querySelector(DOMstring.inputType).value,
+        Desc: document.querySelector(DOMstring.inputDesc).value,
+        Value: document.querySelector(DOMstring.inputValue).value
+      };
+    },
+    getDOMstring: function() {
+      return DOMstring;
+    }
+  };
+})();
 
 //Санхүүтэй ажиллах controller
 var financeController = (function() {})();
 
 //Холбогч controller
-var connectController = (function(uiController, financeController) {
+var appController = (function(uiController, financeController) {
+  var DOM = uiController.getDOMstring();
   function ctrlAddItem() {
-    //Хийсэн үйлдлийг харуулна
-    //Санхүүгийн тооцоо хийнэ
-    //
+    //Оруулах өгөгдлийг дэлгэцнээс олж авна.
+    console.log(uiController.inputValue());
+    //Оруулсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэндээ хадгална.
+    //Олж авсан өгөгдлүүдээ вэб дээр тохирох хэсэгт харуулна.
+    //Төсвийг тооцно.
+    //Эцсийн үлдэгдлийг дэлгэцэнд гаргана.
   }
 
-  document.querySelector(".add__btn").addEventListener("click", function() {
+  document.querySelector(DOM.addBtn).addEventListener("click", function() {
     ctrlAddItem();
   });
 
